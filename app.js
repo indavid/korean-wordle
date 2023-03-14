17daa40c7ded61d0b5a23176c5ec027376103236
“indavid” <david@gmail.com>
2023-03-13 17:24:19 -0700

    Add localStorage capability, daily answer reset, game win/lost toast, input letter/word animation

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const ANSWER_LENGTH = 5; // How many lettters is the answer
 const ROUNDS = 6; // How many guesses a player gets
 
-// Contains Game State
+/** GameState class - Contains Game State / Represents Controller/Model **/
 class GameState {
   constructor() {
     this.rows = ROUNDS; // Number of Guesses
     this.cols = ANSWER_LENGTH; // Length of Answer
+    // Set arrays of board, style, and animation
+    const storedBoard = JSON.parse(localStorage.getItem("board"));
+    this.board = storedBoard
+      ? storedBoard
+      : Array.from(Array(this.rows), () => new Array(this.cols).fill(null));
+    const storedStyleBoard = JSON.parse(localStorage.getItem("styleBoard"));
+    this.styleBoard = storedStyleBoard
+      ? storedStyleBoard
+      : Array.from(Array(this.rows), () => new Array(this.cols).fill(null));
+    const storedAnimationBoard = JSON.parse(
+      localStorage.getItem("animationBoard")
+    );
+    this.animationBoard = storedAnimationBoard
+      ? storedAnimationBoard
+      : Array.from(Array(this.rows), () => new Array(this.cols).fill(null));
+  }
+
+  // reset everything
+  reset() {
     this.board = Array.from(Array(this.rows), () =>
       new Array(this.cols).fill(null)
     );
@@ -16,58 +35,97 @@ class GameState {
       new Array(this.cols).fill(null)
     );
   }
+
   // Getter method for gameState 2-D Array
   getBoard() {
+    localStorage.setItem("board", JSON.stringify(this.board));
     return this.board;
   }
   // Add Letter to Array
   addLetter(rows, cols, letter) {
     this.board[rows][cols] = letter;
+    localStorage.setItem("board", JSON.stringify(this.board));
   }
   // Delete Letter to Array
   deleteLetter(rows, cols) {
     this.board[rows][cols] = null;
+    localStorage.setItem("board", JSON.stringify(this.board));
   }
   // Getter for styleBoard
   getStyleBoard() {
+    localStorage.setItem("styleBoard", JSON.stringify(this.styleBoard));
     return this.styleBoard;
   }
   // Add Style to Array
   addStyle(rows, cols, style) {
     this.styleBoard[rows][cols] = style;
+    localStorage.setItem("styleBoard", JSON.stringify(this.styleBoard));
   }
   // Delete Style to Array
   deleteStyle(rows, cols) {
     this.styleBoard[rows][cols] = null;
+    localStorage.setItem("styleBoard", JSON.stringify(this.styleBoard));
   }
   // Getter for styleBoard
   getAnimationBoard() {
+    localStorage.setItem("animationBoard", JSON.stringify(this.animationBoard));
     return this.animationBoard;
   }
   // Add Style to Array
   addAnimation(rows, cols, animation) {
     this.animationBoard[rows][cols] = animation;
+    localStorage.setItem("animationBoard", JSON.stringify(this.animationBoard));
   }
   // Delete Style to Array
   resetAnimation() {
     this.animationBoard = Array.from(Array(this.rows), () =>
       new Array(this.cols).fill(null)
     );
+    localStorage.setItem("animationBoard", JSON.stringify(this.animationBoard));
   }
 }
 
-// Contains Game Logic
+/** GameController class - Contains Game Logic / Represents the Controller **/
 class GameController {
-  constructor(gameStateInput) {
+  constructor(gameStateInput, timeUntilResetInput) {
     this.gameState = gameStateInput;
-    this.wordList = wordList;
-    this.answer = "";
     this.answerLength = ANSWER_LENGTH;
     this.rounds = ROUNDS;
-    this.currentRound = 0;
-    this.currentGuess = "";
-    this.isGameRunning = true;
-    this.isGameWon = false;
+    this.wordList = wordList;
+    // Have to store all of these in localStorage
+    // Retrive a new answer for every 24 hours
+    const now = new Date();
+    if (!localStorage.getItem("timeStarted")) {
+      localStorage.setItem("timeStarted", JSON.stringify(now.getTime()));
+    }
+    const timeElapsed = now.getTime() - localStorage.getItem("timeStarted");
+    localStorage.setItem("timeElapsed", JSON.stringify(timeElapsed));
+    let storedAnswer = JSON.parse(localStorage.getItem("answer"));
+    if (!storedAnswer) {
+      this.answer = this.loadAnswer();
+    } else {
+      // time to reset answer and the whole game in ms: 1 min
+      if (timeElapsed >= 60000) {
+        localStorage.setItem("timeStarted", JSON.stringify(now.getTime()));
+        this.resetGame();
+      } else {
+        this.answer = storedAnswer;
+      }
+    }
+    console.log(this.answer);
+
+    // Store other variables in local Storage
+    let storedCurrentRound = JSON.parse(localStorage.getItem("currentRound"));
+    this.currentRound = storedCurrentRound ? storedCurrentRound : 0;
+
+    let storedCurrentGuess = JSON.parse(localStorage.getItem("currentGuess"));
+    this.currentGuess = storedCurrentGuess ? storedCurrentGuess : "";
+
+    let storedIsGameRunning = JSON.parse(localStorage.getItem("isGameRunning"));
+    this.isGameRunning = storedIsGameRunning ? storedIsGameRunning : true;
+
+    let storedIsGameWon = JSON.parse(localStorage.getItem("isGameWon"));
+    this.isGameWon = storedIsGameWon ? storedIsGameWon : false;
   }
   // Getter for isGameWon
   getIsGameWon = () => {
@@ -88,14 +146,30 @@ class GameController {
   getAnimationBoard = () => {
     return this.gameState.getAnimationBoard();
   };
-  // Load answer
+  // Load answer for every 24 hours
   loadAnswer() {
     try {
-      this.answer = this.wordList[Math.floor(Math.random() * 38, 698)];
-      console.log(this.answer);
+      let word =
+        this.wordList[Math.floor(Math.random() * this.wordList.length)];
+      localStorage.setItem("answer", JSON.stringify(word));
+      // return either stored word or new generated answer
+      return word;
     } catch (error) {
       console.error("Could not load answer");
     }
+  }
+  // reset game once a certain amount of time has passed since game started
+  resetGame() {
+    // reload answer
+    this.answer = this.loadAnswer();
+    // reset all of the other variables
+    localStorage.setItem("currentRound", JSON.stringify(0));
+    localStorage.setItem("currentGuess", JSON.stringify(""));
+    localStorage.setItem("isGameRunning", JSON.stringify(true));
+    localStorage.setItem("isGameWon", JSON.stringify(false));
+
+    // reset game state
+    this.gameState.reset();
   }
   // Check if input is an English letter
   isLetter(letter) {
@@ -144,9 +218,11 @@ class GameController {
   addLetter(letter) {
     if (this.currentGuess.length < this.answerLength) {
       this.currentGuess += letter;
+      localStorage.setItem("currentGuess", JSON.stringify(this.currentGuess));
     } else {
       this.currentGuess =
         this.currentGuess.substring(0, this.answerLength - 1) + letter;
+      localStorage.setItem("currentGuess", JSON.stringify(this.currentGuess));
     }
     this.gameState.addLetter(
       this.currentRound,
@@ -158,6 +234,11 @@ class GameController {
       this.currentGuess.length - 1,
       "filled"
     );
+    this.gameState.addAnimation(
+      this.currentRound,
+      this.currentGuess.length - 1,
+      "animateFilled"
+    );
   }
   // Delete letter from gameState array
   deleteLetter() {
@@ -165,7 +246,9 @@ class GameController {
       0,
       this.currentGuess.length - 1
     );
+    localStorage.setItem("currentGuess", JSON.stringify(this.currentGuess));
     this.gameState.deleteLetter(this.currentRound, this.currentGuess.length);
+    this.gameState.deleteStyle(this.currentRound, this.currentGuess.length);
     this.gameState.deleteStyle(this.currentRound, this.currentGuess.length);
   }
   // Call after animation has been triggered
@@ -193,7 +276,7 @@ class GameController {
     } else if (!this.wordList.includes(this.currentGuess)) {
       console.log("not a valid word");
       for (let i = 0; i < this.answerLength; i++) {
-        this.gameState.addAnimation(this.currentRound, i, "invalid");
+        this.gameState.addAnimation(this.currentRound, i, "animateInvalid");
       }
       return;
     }
@@ -207,6 +290,7 @@ class GameController {
     for (let i = 0; i < this.answerLength; i++) {
       if (guessParts[i] === answerParts[i]) {
         this.gameState.addStyle(this.currentRound, i, "correct");
+        this.gameState.addAnimation(this.currentRound, i, "animateSubmit");
         answerMap[guessParts[i]]--;
       }
     }
@@ -216,23 +300,32 @@ class GameController {
         // do nothing
       } else if (answerMap[guessParts[i]] && answerMap[guessParts[i]] > 0) {
         this.gameState.addStyle(this.currentRound, i, "close");
+        this.gameState.addAnimation(this.currentRound, i, "animateSubmit");
         answerMap[guessParts[i]]--;
         this.isGameWon = false;
+        localStorage.setItem("isGameWon", JSON.stringify(this.isGameWon));
       } else {
         this.gameState.addStyle(this.currentRound, i, "wrong");
+        this.gameState.addAnimation(this.currentRound, i, "animateSubmit");
         this.isGameWon = false;
+        localStorage.setItem("isGameWon", JSON.stringify(this.isGameWon));
       }
     }
     // advance to next guess
     this.currentRound++;
+    localStorage.setItem("currentRound", JSON.stringify(this.currentRound));
     this.currentGuess = "";
+    localStorage.setItem("currentGuess", JSON.stringify(this.currentGuess));
 
     // Check if game is over - Play out Win or Lose conditions
     if (this.isGameWon) {
       this.isGameRunning = false;
+      localStorage.setItem("isGameRunning", JSON.stringify(this.isGameRunning));
       this.currentRound = 6;
+      localStorage.setItem("currentRound", JSON.stringify(this.currentRound));
     } else if (this.currentRound === this.rounds) {
       this.isGameRunning = false;
+      localStorage.setItem("isGameRunning", JSON.stringify(this.isGameRunning));
     }
   }
   // Handle game logic of keyboard event from user
@@ -252,19 +345,32 @@ class GameController {
   }
 }
 
+/** ScreenController class - Contains Screen Logic / Renders the View **/
 class ScreenController {
-  constructor(gameController) {
+  constructor(gameControllerInput) {
     this.body = document.body;
     this.boardDiv = document.querySelector(".board");
     this.keyButtons = document.querySelectorAll(".key");
-    this.gameController = gameController;
-    this.spinner = document.querySelector(".spinner");
+    this.gameController = gameControllerInput;
+    this.toast = document.querySelector(".toast");
   }
-  // Set loading icon spinning
-  setLoading(isLoading) {
-    isLoading
-      ? (this.spinner.style.visibility = "visible")
-      : (this.spinner.style.visibility = "hidden");
+  // Pass in a boolean that loads the appropriate toast
+  setToast(hasWon) {
+    const text = document.createElement("p");
+    text.className = "toastText";
+    this.toast.style.display = "block";
+    if (hasWon) {
+      text.innerText = "Splendid!";
+      this.toast.appendChild(text);
+      setTimeout(() => {
+        this.toast.removeChild(text);
+        this.toast.style.display = "none";
+      }, 3000);
+    } else {
+      text.innerText =
+        " Better luck tomorrow..." + "\n Answer: " + this.gameController.answer;
+      this.toast.appendChild(text);
+    }
   }
   // Render View based off of Model given by Controller
   renderBoard() {
@@ -295,31 +401,40 @@ class ScreenController {
   listen() {
     // Listen to keyboard events and check if game is over
     this.body.addEventListener("keydown", ({ key, preventDefault }) => {
-      this.gameController.handleEvent(key, preventDefault);
-      this.renderBoard();
-      console.log(
-        this.gameController.getIsGameRunning() +
-          " and " +
-          this.gameController.getIsGameWon()
-      );
-      if (!this.gameController.getIsGameRunning()) {
-        if (this.gameController.getIsGameWon()) {
-          setTimeout(() => alert("You won! :)"), 500);
-        } else {
-          setTimeout(() => alert("You lose. :("), 500);
+      // Make sure game is still running before processing events
+      if (this.gameController.getIsGameRunning()) {
+        this.gameController.handleEvent(key, preventDefault);
+        this.renderBoard();
+        if (!this.gameController.getIsGameRunning()) {
+          if (this.gameController.getIsGameWon()) {
+            setTimeout(() => {
+              this.setToast(true);
+            }, 500);
+          } else {
+            setTimeout(() => {
+              this.setToast(false);
+            }, 500);
+          }
         }
       }
     });
     // Listen to button events and check if game is over
     this.keyButtons.forEach((key) => {
       key.addEventListener("click", () => {
-        this.gameController.handleEvent(key.innerText, null);
-        this.renderBoard();
-        if (!this.gameController.getIsGameRunning()) {
-          if (this.gameController.getIsGameWon()) {
-            setTimeout(() => alert("You won! :)"), 500);
-          } else {
-            setTimeout(() => alert("You lose. :("), 500);
+        // Make sure game is still running before processing events
+        if (this.gameController.getIsGameRunning()) {
+          this.gameController.handleEvent(key.innerText, null);
+          this.renderBoard();
+          if (!this.gameController.getIsGameRunning()) {
+            if (this.gameController.getIsGameWon()) {
+              setTimeout(() => {
+                this.setToast(true);
+              }, 500);
+            } else {
+              setTimeout(() => {
+                this.setToast(false);
+              }, 500);
+            }
           }
         }
       });
@@ -331,7 +446,6 @@ function init() {
   const state = new GameState();
   console.log(state.board);
   const game = new GameController(state);
-  game.loadAnswer();
   const screen = new ScreenController(game);
   screen.renderBoard();
   screen.listen();
